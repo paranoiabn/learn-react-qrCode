a995c6980c18b5c2f2b79cf024eda80d8d522df9
Paranoia <cryptoamoral@gmail.com>
2025-11-21 17:16:49 +0100

    mm

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  NavigationType,
-  RouterProvider,
-} from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './Layout';
 
